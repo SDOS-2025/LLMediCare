3f7b617bc63b670badfe078f5b41b5b4d68aca24
Debjit-Banerji <banerjidebjit@gmail.com>
2025-03-14 19:46:17 +0530

    Improved Appoitments UI

--- frontend/src/pages/ChatInterface.jsx
+++ frontend/src/pages/ChatInterface.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import { Header } from '../components/Header';
+import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import { Link } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Header } from '../components/Header';
+import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 
 export default function Home () {
